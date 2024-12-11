6839800948f7f53cc455200bcddec3a47e4b0409
Amadéo Berthelot <78102463+amadeo-dev@users.noreply.github.com>
2024-12-11 11:42:08 +0100

    Commit message

--- epreuves_logiques.py
+++ epreuves_logiques.py
@@ -1,3 +1,4 @@
+import random
 
 def suiv(joueur):
     if joueur == 0:
@@ -13,15 +14,16 @@ def grille_vide():
 def affiche_grille(grille, message):
     print(message)
     for ligne in grille:
-        print("|" + "|".join(ligne) + "|")
+        print("| " + " | ".join(ligne) + " |")
     print("-" * 10)
 
 
 def demande_position():
-    position = input("Entrez la position (ligne,colonne) entre 1 et 3 (ex: 1,2) :")
-    ligne, colonne = map(int, position.split(','))
-    if 0 < ligne <= 3 and 0 <= colonne <= 3:
-        return ligne-1, colonne-1
+    position = input("Entrez la position (ligne,colonne) entre 1 et 3 (ex: 1,2) : ")
+    ligne, colonne = position.split(',')
+    ligne, colonne = int(ligne), int(colonne)
+    if 0 < ligne <= 3 and 0 < colonne <= 3:
+        return ligne - 1, colonne - 1
     else:
         print("Position invalide")
 
@@ -40,13 +42,33 @@ def init():
     affiche_grille(grille, "Découvrez votre grille de jeu avec vos bateaux :")
 
 def tour(joueur, grille_tirs_joueur, grille_adversaire):
-
-    affiche_grille(grille_jouer, "C'est à votre tour de faire feu !:")
+    if joueur == 0:
+        affiche_grille(grille_tirs_joueur, "C'est à votre tour de faire feu !:")
+        tirl, tirc = demande_position()
+        if grille_adversaire[tirl][tirc] == 'B':
+            grille_tirs_joueur[tirl][tirc] = 'x'
+        else:
+            grille_tirs_joueur[tirl][tirc] = '.'
+    if joueur == 1:
+        tirl, tirc = random.randint(0,2), random.randint(0,2)
+        if grille_adversaire[tirl][tirc] == 'B':
+            grille_tirs_joueur[tirl][tirc] = 'x'
+        else:
+            grille_tirs_joueur[tirl][tirc] = '.'
 
 def gagne(grille_tirs_joueur):
-    pass
+    wincompte = 0
+    for i in grille_tirs_joueur:
+        if grille_tirs_joueur[i] == 'x':
+            wincompte += 1
+    if wincompte == 2:
+        return True
+    if wincompte < 2:
+        return False
 
 def jeu_bataille_navale():
     print("Chaque joueur doit placer 2 bateaux sur une grille de 3x3.")
     init()
 
+
+print(jeu_bataille_navale())
