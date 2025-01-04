c3fa83bacb1a551b531a16b738c7440db5380832
jackyzhang <jacky.zhang@efrei.net>
2025-01-05 00:16:39 +0100

    wéé

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from fonctions_utiles import *
 
 def jeu():
     introduction()
-    ekip = composer_equipe()    #présentation et crétion des équipes
+    ekip = composer_equipe()    #présentation et création des équipes
     cles = 0                    # initialisation du nombre de clés à 0
 
     while cles < 3:             # On répète tant que l'équipe n'a pas trois clés
@@ -32,6 +32,7 @@ def jeu():
             joueur_choisi['cles_gagnees'] += 1
             cles += 1
 
+    print("L'épreuve de la Salle au trésor commence !")
     final = salle_De_Tresor()  # une fois les 3 clés obtenues -> salle tresor
     if final == True:
         "L'équipe remporte"
